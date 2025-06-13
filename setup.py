695fba7988c6697e3b7f4a69e83205adceb1a925
julianbrandt <julian@julianbrandt.dk>
2025-06-13 22:45:35 +0200

    modified new meme dimensions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="MemePy",
-    version="1.2.2",
+    version="1.2.3",
     description="Meme Generator for python",
     long_description=readme,
     long_description_content_type="text/markdown",
